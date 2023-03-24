0b6630ec293b985854c97241cb4931812c40d804
CarlosVarasAlonso <carlos.varalo@educa.jcyl.es>
2023-03-24 18:36:54 +0100

    Segundo commit desde eclipse

--- Helloworld/src/helloworld/main.java
+++ Helloworld/src/helloworld/main.java
@@ -2,9 +2,10 @@ package helloworld;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		System.out.println("Hello world32");
 	}
 
 }
