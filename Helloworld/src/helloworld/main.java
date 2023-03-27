3277723a29d988cf337210d66fccf90787ff386f
CarlosVarasAlonso <carlos.varalo@educa.jcyl.es>
2023-03-27 18:56:35 +0200

    archivos

--- Helloworld/src/helloworld/main.java
+++ Helloworld/src/helloworld/main.java
@@ -5,8 +5,8 @@ public class main {
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		System.out.println("Hello world32");
-		System.out.println("Hello world");
+		System.out.println("Hello world2");
+		System.out.println("Hello world3");
 	}
 
 }
